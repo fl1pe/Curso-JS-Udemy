884fa13f71dc2b6f44dcd6fe308da4bde02cfa14
Felipe Bezerra <felipbsousa036@gmail.com>
2022-06-19 15:20:15 -0400

    exercicio-fizzBuzz

---
+++ JavaScrip - Lógica de programação/aula38/exercicio-03.js
@@ -0,0 +1,21 @@
+/* Escreva uma função que recebe um número e 
+retorne o seguinte:
+Número é divisível por 3 = Fizz
+Número é divisível por 5 = Buzz
+Número é divisível por 3 e 5 = FizzBuzz
+Checar se o número é realmente um número
+Use a função com números de 0 a 100
+*/
+
+function fizzBuzz(numero) {
+    if (typeof numero !== "number") return numero;
+
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return 'FizzBuzz';
+    } else if(numero % 3 === 0){
+        return 'Fizz';
+    } else if(numero % 5 === 0){
+        return 'Buzz';
+    } else return numero;
+}
+f
