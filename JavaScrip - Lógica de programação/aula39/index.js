ff6cb02e4bfdee39e8ec848a748621ff87ea2171
Felipe Bezerra <felipbsousa036@gmail.com>
2022-06-21 20:26:54 -0400

    try-catch-finally

--- JavaScrip - Lógica de programação/aula39/index.js
+++ JavaScrip - Lógica de programação/aula39/index.js
@@ -1,4 +1,4 @@
-/* Tratando e lançando erros (try, catch, throw)*/
+/* Tratando e lançando erros (try, catch, throw), também existe o bloco "finally" que sempre será executado.*/
 
 // try{
 //     console.log(naoExiste);
@@ -6,15 +6,51 @@
 //     console.log('Variavel nao existe');
 // }
 
-function soma(x, y) {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        throw new ReferenceError('x e y precisao ser numeros');
+// function soma(x, y) {
+//     if (typeof x !== 'number' || typeof y !== 'number') {
+//         throw new ReferenceError('x e y precisao ser numeros');
+//     }
+//     return x + y;
+// }
+
+// try {
+//     console.log(soma('45',2));
+// } catch(error){
+//     console.log('Deu um erro irmão');
+// }
+
+/* try {
+    console.log("Olá, Mundo!", teste); // Será gerado um erro.
+} catch (e){
+    console.log('Erro foi tratado.');
+} finally{
+    console.log('FINALLY: Eu sempre sou executado.');
+} */
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.');
     }
-    return x + y;
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        seconds: '2-digit',
+        hour12: false
+    });
+
 }
 
 try {
-    console.log(soma('45',2));
-} catch(error){
-    console.log('Deu um erro irmão');
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora();
+    console.log(hora);
+} catch(e){
+    // Tratar erro
+} finally {
+    console.log('Olá!');
 }
