82975873d7c84edf56cc9813e34e045bb1a1bfe4
Felipe Bezerra <felipbsousa036@gmail.com>
2022-06-26 21:40:28 -0400

    IIFE -> Immediately invoked function express

---
+++ JavaScript Funções (Avançado)/aula47/index.js
@@ -0,0 +1,16 @@
+/* IIFE -> Immediately invoked function express
+Funções imediatas
+
+(function(){
+
+})();
+
+*/
+
+(function () {
+
+    function falaNome() {
+        console.log('Felipe')
+    }
+    falaNome();
+})();
