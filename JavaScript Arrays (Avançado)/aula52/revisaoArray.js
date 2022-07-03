bbc4e58c58d1df6ec364cca81e995f686a633a8f
Felipe Bezerra <felipbsousa036@gmail.com>
2022-07-03 14:52:21 -0400

    revisao-array

---
+++ JavaScript Arrays (Avançado)/aula52/revisaoArray.js
@@ -0,0 +1,7 @@
+//                0         1        2         3       4        5
+const nomes = ["Felipe", "Alana", "Maria", "Luiza", "Pedro", "João"];
+
+const nomeCompleto = "Felipe Bezerra de Sousa Ferreira";
+const nome = nomeCompleto.split(" ");
+
+console.log(nome.join(" "));
