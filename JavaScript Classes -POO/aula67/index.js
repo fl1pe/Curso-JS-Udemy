2ec004c45e415245fc20ad04ad0bc66569e4792a
Felipe Bezerra <felipbsousa036@gmail.com>
2022-07-30 22:38:46 -0400

    Usando classe (validando CPF)

---
+++ JavaScript Classes -POO/aula67/index.js
@@ -0,0 +1,47 @@
+// Usando classes ( Validando CPF)
+
+const cpfTeste = "705.484.450-52";
+
+class ValidaCpf {
+    constructor(cpf) {
+        Object.defineProperty(this, "cpfLimpo", {
+            enumerable: true,
+            value: cpf.replace(/\D+/g, ""),
+        });
+    }
+
+    sequencia() {
+        return this.cpfLimpo[0].repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCpf = cpfSemDigitos + digito1 + digito2;
+    }
+
+    geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let i of cpfSemDigitos) {
+            total += reverso * Number(i);
+            reverso--;
+        }
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : "0";
+    }
+
+    valida() {
+        if (typeof this.cpfLimpo === "undefined") return false;
+        if (this.cpfLimpo.length != 11) return false;
+        if (this.sequencia()) return false;
+        this.geraNovoCpf();
+
+        return this.novoCpf === this.cpfLimpo;
+    }
+}
+
+const testeFuncao = new ValidaCpf("616.068.023-48");
+console.log(testeFuncao.valida());
